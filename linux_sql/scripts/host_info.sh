b14df841302c8f92b9528b4a0f555d002e872d41
Harsh Patel <harshworkspace97@gmail.com>
2021-10-25 15:56:05 +0000

    host hardware info

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 hostname=$(hostname -f)
-lscpu_out=$(lscpu)
-cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
-cpu_model=$(echo "$lscpu_out"  | egrep 'Model name' | awk '{print $3 $4 $5 $6 $7 $8 $9 }' | xargs)
-cpu_mhz=$(echo "$lscpu_out"  | egrep 'CPU MHz' | awk '{print $3 }' | xargs)
-l2_cache=$(echo "$lscpu_out"  | egrep 'L2 cache' | awk '{print $3 }' | xargs)
-total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2 $3}'| xargs)
+lscpu_out=`lscpu`
+cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "^Model:" | awk '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU\sMHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out" | egrep "^L2\scache:" | awk '{print $3}' | xargs)
+total_mem=$(cat /proc/meminfo | egrep "MemTotal:" | awk '{print $2}' | xargs)
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 echo $hostname
 echo $cpu_number
 echo $cpu_architecture
@@ -15,5 +16,6 @@ echo $cpu_model
 echo $cpu_mhz
 echo $l2_cache
 echo $total_mem
+echo $timestamp
 
 
