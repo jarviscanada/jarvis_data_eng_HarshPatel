8ac8b4e0efa74e7d2296d3893ce6a6bd96961975
harsh <h_el2019@live.concordia.ca>
2021-10-27 11:09:28 -0400

    updated info and usage file

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+#Error message for missing arguments
+if [ "$#" -ne 6 ]; then
+    echo "Illegal number of parameters. Please provide psql host, port number, database name, user and password"
+    exit 1
+fi
+
 hostname=$(hostname -f)
 lscpu_out=$(lscpu)
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
@@ -8,12 +20,12 @@ cpu_model=$(echo "$lscpu_out"  | egrep 'Model name' | awk '{print $3 $4 $5 $6 $7
 cpu_mhz=$(echo "$lscpu_out"  | egrep 'CPU MHz' | awk '{print $3 }' | xargs)
 l2_cache=$(echo "$lscpu_out"  | egrep 'L2 cache' | awk '{print $3 }' | xargs)
 total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2 $3}'| xargs)
-echo $hostname
-echo $cpu_number
-echo $cpu_architecture
-echo $cpu_model
-echo $cpu_mhz
-echo $l2_cache
-echo $total_mem
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
+#Query to insert data into host_info table
+insert_stmnt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp) VALUES('$hostname', $cpu_number, '$cpu_architecture', $cpu_model, $cpu_mhz, $l2_cache, $total_mem, '$timestamp');"
 
+#Executing Insert statement through psql CLI tool
+export PGPASSWORD=$psql_password
+psql -h localhost -p 5432 -U postgres -d host_agent -c "$insert_stmnt"
+exit 0
